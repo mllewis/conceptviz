66cd96306b8fe55e647ffc0516c3a47874e5e263
Molly Lewis <mollyllewis@gmail.com>
2017-07-25 15:07:44 -0500

    dist stuff

---
+++ data/supplementary_data/cultural_sim_measures/captial_dist.R
@@ -0,0 +1,71 @@
+###########Get dists between all captials##########
+
+library(tidyverse)
+library(forcats)
+
+## get countries in google
+countries <- read.csv("../../data/supplementary_data/iso_3166_2_countries.csv") %>%
+  rename(country = Common.Name,
+         country_code = ISO.3166.1.2.Letter.Code) %>%
+  select(country, country_code) %>%
+  mutate(country_code = as.character(country_code)) %>% # for joining, below
+  group_by(country_code) %>%
+  slice(1)
+
+
+geo_codes <- read_csv("../../data/supplementary_data/geo_codes.csv")
+
+d <- list.files("../../data/summary_data/") %>%
+  purrr::map(function(x) paste0("../../data/summary_data/", x)) %>% 
+  purrr::map(read_feather) %>% 
+  bind_rows() 
+
+d_codes <- left_join(d, geo_codes, by = c("country" = "countries")) %>%
+  left_join(countries) %>%
+  distinct(country, country_code)
+
+#write_csv(d_codes, "../../data/supplementary_data/all_google_countries.csv")
+g_cities = read_csv("../../data/supplementary_data/all_google_countries.csv")
+
+
+## get capital cities
+data(maps::world.cities) # load map data
+
+all_capitals <-  world.cities %>%
+  filter(capital == 1) %>%
+  mutate(country.etc = fct_recode(country.etc,
+                                  "United Kingdom" = "UK",
+                                  "United States" = "USA",
+                                  "Korea, South" = "Korea South",
+                                  "China, Republic of (Taiwan)" = "Taiwan",
+                                  "Serbia" = "Serbia and Montenegro"))
+                                  
+
+g_cities_data <- g_cities %>%
+  left_join(all_capitals, by = c("country" = "country.etc")) %>%
+  select(-capital, -pop) %>%
+  rename(capital_name = name)
+
+#write_csv(g_cities_data, "../../data/supplementary_data/all_google_capitals.csv")
+g_capitals <- read_csv("../../data/supplementary_data/all_google_capitals.csv")
+
+## get pairwise capital dist
+g_dists <- geosphere::distm(cbind(g_capitals$long, g_capitals$lat)) # in meters
+rownames(g_dists) <- g_capitals$capital_name
+colnames(g_dists) <- g_capitals$capital_name
+
+all_dists <- reshape2::melt(g_dists) %>%
+  rename(city1 = Var1,
+         city2 = Var2,
+         dist_meters = value)
+
+all_dists_with_countries = all_dists %>%
+  left_join(g_capitals %>% select(country, capital_name), by = c("city1" = "capital_name")) %>%
+  left_join(g_capitals %>% select(country, capital_name), by = c("city2" = "capital_name")) %>%
+  rename(country1 = country.x,
+         country2 = country.y) %>%
+  select(country1, country2, city1, city2, dist_meters)
+
+write_csv(all_dists_with_countries, "../../data/supplementary_data/all_google_capital_dists.csv")
+  
+  
